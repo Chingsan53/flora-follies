376ff7021c58de795b0814237bb9c438661b7f12
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-03-31 16:31:32 -0700

    added more flowers data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -117,6 +117,7 @@ const Homepage = () => {
             image: "./img/iris.webp",
             name: "Iris",
             category: "",
+            summary: "striking flowers known for their unique shape and vivid colors, ranging from deep purples and blues to yellows and whites. These perennials typically bloom in late spring to early summer.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -134,6 +135,7 @@ const Homepage = () => {
             image: "./img/pansy.jpeg",
             name: "Pansy",
             category: "",
+            summary: "popular for their charming, colorful flowers, which can display a wide range of hues including purple, blue, red, gold, and white, often with striking patterns.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -151,6 +153,7 @@ const Homepage = () => {
             image: "./img/viola.webp",
             name: "Viola",
             category: "",
+            summary: "also known as \"heartsease,\" are small, resilient flowers, known for their ability to bloom in cool temperatures. They come in a variety of colors, often with two-tone patterns. ",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -168,6 +171,7 @@ const Homepage = () => {
             image: "./img/forsythia.jpeg",
             name: "Forsythia",
             category: "",
+            summary: "often one of the first signs of spring, is known for its vibrant yellow blossoms that cover its branches before the leaves appear.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -185,6 +189,7 @@ const Homepage = () => {
             image: "./img/lilac.jpg",
             name: "Lilac",
             category: "",
+            summary: "renowned for their fragrant and lush panicles of flowers, usually in shades of purple, but also available in white and pink.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -202,6 +207,7 @@ const Homepage = () => {
             image: "./img/cherry-blossom.webp",
             name: "Cherry Blossom",
             category: "",
+            summary: "Cherry Blossoms, iconic for their delicate and picturesque beauty, are synonymous with spring. These flowers, ranging from white to pale pink, bloom in clusters on the branches of cherry trees.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -219,6 +225,7 @@ const Homepage = () => {
             image: "./img/magnolia.jpeg",
             name: "Magnolia",
             category: "",
+            summary: "known for their large and impressive blooms, are a standout feature of early spring. These flowers come in shades of white, pink, purple, and yellow, and are known for their rich fragrance.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -236,6 +243,7 @@ const Homepage = () => {
             image: "./img/azalea.webp",
             name: "Azalea",
             category: "",
+            summary: "known for their vibrant and showy flowers, blooming in shades of pink, red, purple, white, and orange. These flowering shrubs, part of the Rhododendron family, are a highlight of spring gardens.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -253,6 +261,7 @@ const Homepage = () => {
             image: "./img/rhododendron.jpeg",
             name: "Rhododendron",
             category: "",
+            summary: "Rhododendrons are known for their stunning, large blooms that appear in late spring. These evergreen shrubs produce flowers in a range of colors including pink, purple, red, white, and yellow. ",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -267,9 +276,10 @@ const Homepage = () => {
         },
         {
             id: 16,
-            image: "",
+            image: "./img/anemone.jpeg",
             name: "Anemone",
             category: "",
+            summary: "also known as windflowers, are celebrated for their delicate, often brightly colored flowers. These spring-blooming perennials come in various hues like pink, white, blue, and red.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -284,9 +294,10 @@ const Homepage = () => {
         },
         {
             id: 17,
-            image: "",
+            image: "./img/bluebell.jpeg",
             name: "Bluebell",
             category: "",
+            summary: "enchanting flowers known for their vivid blue, bell-shaped blooms that form delicate clusters. They typically flower in late spring and are often found carpeting woodland floors, creating a stunning blue-purple 'sea'.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -301,9 +312,10 @@ const Homepage = () => {
         },
         {
             id: 18,
-            image: "",
+            image: "./img/ranunculus.jpeg",
             name: "Ranunculus",
             category: "",
+            summary: "known for their lush, multi-layered petals and come in a variety of vibrant colors, including pink, red, orange, yellow, and white. These spring-blooming perennials are appreciated for their rose-like appearance and are often used in bouquets and floral arrangements. ",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -318,9 +330,10 @@ const Homepage = () => {
         },
         {
             id: 19,
-            image: "",
+            image: "./img/peony.jpeg",
             name: "Peony",
             category: "",
+            summary: "large, lush blooms and rich colors, ranging from deep pinks and reds to soft whites and yellows. Blooming in late spring to early summer, these perennials are a favorite in gardens and as cut flowers.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -335,9 +348,10 @@ const Homepage = () => {
         },
         {
             id: 20,
-            image: "",
+            image: "./img/allium.webp",
             name: "Allium",
             category: "",
+            summary: "known for their striking globe-shaped flowers, bloom in late spring to early summer. These ornamental onions display clusters of small flowers in shades of purple, pink, white, and blue atop tall stems.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -352,9 +366,10 @@ const Homepage = () => {
         },
         {
             id: 21,
-            image: "",
+            image: "./img/forget-me-not.jpg",
             name: "Forget-me-not",
             category: "",
+            summary: "charming small flowers known for their vivid blue color with yellow centers. These perennial plants bloom in spring and are often found in woodland settings and along stream banks.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -369,9 +384,10 @@ const Homepage = () => {
         },
         {
             id: 22,
-            image: "",
+            image: "./img/columbine.jpeg",
             name: "Columbine",
             category: "",
+            summary: "known for their distinctive, bell-shaped flowers with spurred petals, blooming in various shades including purple, blue, pink, red, and white. These perennials flower in late spring to early summer and are popular for their delicate, fairy-like appearance.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -386,9 +402,10 @@ const Homepage = () => {
         },
         {
             id: 23,
-            image: "",
+            image: "./img/camellia.jpeg",
             name: "Camellia",
             category: "",
+            summary: "known for their glossy, evergreen leaves and large, exquisite flowers that bloom in shades of red, pink, and white. These flowers appear in late winter to early spring, providing a splash of color in colder months.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -403,9 +420,10 @@ const Homepage = () => {
         },
         {
             id: 24,
-            image: "",
+            image: "./img/bleeding-heart.jpeg",
             name: "Bleeding Heart",
             category: "",
+            summary: "known for their unique, heart-shaped flowers that dangle in rows from arching stems. Blooming in spring, these flowers are typically pink or white with a drop at the bottom, resembling a bleeding heart.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -420,9 +438,10 @@ const Homepage = () => {
         },
         {
             id: 25,
-            image: "",
+            image: "./img/poppy.webp",
             name: "Poppy",
             category: "",
+            summary: "known for their vivid and often papery-textured flowers, blooming in a variety of colors like red, orange, pink, and white. These annuals or perennials are famous for their large, solitary blooms.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -437,9 +456,10 @@ const Homepage = () => {
         },
         {
             id: 26,
-            image: "",
+            image: "./img/fox-glove.jpeg",
             name: "Foxglove",
             category: "",
+            summary: "tall spikes of tubular flowers, ranging from purple to white, bloom in early summer. Prefers moist, well-drained soil and partial shade. Symbolizes insincerity.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -454,9 +474,10 @@ const Homepage = () => {
         },
         {
             id: 27,
-            image: "",
+            image: "./img/hellebore.webp",
             name: "Hellebore",
             category: "",
+            summary: "blooms in late winter to early spring with cup-shaped flowers in shades of white, green, pink, and purple. Prefers well-drained soil and partial shade. Symbolizes serenity.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -471,9 +492,10 @@ const Homepage = () => {
         },
         {
             id: 28,
-            image: "",
+            image: "./img/Snapdragon.webp",
             name: "Snapdragon",
             category: "",
+            summary: "showcases vibrant, dragon-shaped flowers in summer. Thrives in full sun and well-drained soil. Symbolizes grace and strength.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -488,9 +510,10 @@ const Homepage = () => {
         },
         {
             id: 29,
-            image: "",
+            image: "./img/wallflower.jpeg",
             name: "Wallflower",
             category: "",
+            summary: "offers clusters of fragrant flowers in spring. Loves sun and well-drained soil. Symbolizes fidelity.",
             details: {
                 soilType: "",
                 sunExposure: "",
@@ -505,9 +528,10 @@ const Homepage = () => {
         },
         {
             id: 30,
-            image: "",
+            image: "./img/lungwort.jpeg",
             name: "Lungwort",
             category: "",
+            summary: "known for spotted leaves and spring blooms in pink, blue, and white. Prefers shade and moist soil. Represents thoughtfulness.",
             details: {
                 soilType: "",
                 sunExposure: "",

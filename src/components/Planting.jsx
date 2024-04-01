a681f36c5abce7a4366bbeb0240e804ad7ffdd57
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-03-31 18:02:51 -0700

    added planting - not finish yet

--- src/components/Planting.jsx
+++ src/components/Planting.jsx
@@ -1,7 +1,58 @@
+import "./Planting.css";
 const Planting = () => {
-    return <div>
-
+  const smallFlower = [
+    {
+      id: 1,
+      name: "Anemone",
+      smallImage: "./small-img/anemone.png",
+    },
+    {
+      id: 2,
+      name: "Crocus",
+      smallImage: "./small-img/crocus.png",
+    },
+    {
+      id: 3,
+      name: "Sunflower",
+      smallImage: "./small-img/sunflower.png",
+    },
+    {
+      id: 4,
+      name: "Tutulip",
+      smallImage: "./small-img/tulip.png",
+    },
+    {
+      id: 5,
+      name: "Hyacinth",
+      smallImage: "./small-img/hyacinth.png",
+    },
+  ];
+  return (
+    <div className="main">
+      <div className="title">
+        <h2>List of the flowers</h2>
+      </div>
+      <div className="big-container">
+        {smallFlower.map((item) => (
+          <div className="container">
+            <div className="flower-list">
+              <div className="flower-pic">
+                <img
+                  className="image-small"
+                  src={item.smallImage}
+                  alt="small-pic"
+                />
+              </div>
+              <div className="flower-name">
+                <h3>{item.name}</h3>
+                <button>Instruction</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-}
+  );
+};
 
 export default Planting;

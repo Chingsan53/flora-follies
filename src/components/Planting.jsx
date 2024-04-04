f15d2ecb2d43c4b6f3494a487e684de5a0677459
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-04-03 18:24:29 -0700

    added my garden

--- src/components/Planting.jsx
+++ src/components/Planting.jsx
@@ -1,18 +1,35 @@
+import "./Planting.css";
 import { useFlowerData } from "./FlowerDataContext";
 const Planting = () => {
   const { selectedFlowers } = useFlowerData();
   return (
-    <div>
-      <h2>My Garden</h2>
-      {selectedFlowers.length === 0 ? (
-        <p>No flowers selected yet.</p>
-      ) : (
-        selectedFlowers.map((item) => (
-          <div key={item.id}>
-            <p>{item.name}</p>
-          </div>
-        ))
-      )}
+    <div className="garden">
+      <div className="title">
+        <h2>My Garden</h2>
+      </div>
+      <div className="flower-list">
+        {selectedFlowers.length === 0 ? (
+          <p>You have not planted any flower yet</p>
+        ) : (
+          selectedFlowers.map((item) => (
+            <div className="flower-tracker">
+              <div className="">
+                <img
+                  className="image-small"
+                  src={item.image}
+                  alt="small-flower"
+                />
+              </div>
+              <div className="flower-name" key={item.id}>
+                <h3>{item.name}</h3>
+                <div className="tracker-info">
+                  <p>{item.weather}</p>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };

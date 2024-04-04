a4841a7d1f5e10421d78417a97364dd5b52332c3
Chingsan53 <cly53@csu.fullerton.edu>
2024-04-04 12:31:49 -0700

    fixed the garden css

--- src/small-components/Note.jsx
+++ src/small-components/Note.jsx
@@ -2,11 +2,18 @@ import { MdDeleteForever } from "react-icons/md";
 import "./Note.css";
 const Note = () => {
   return (
-    <div className="note-pad">
-      <span>My note is very interesting</span>
-      <div className="note-footer">
-        <small>04/04/2024</small>
-        <MdDeleteForever className="delete-icon" size="1.3em" />
+    <div className="note-whole">
+      <div className="note-title">
+        <div className="button-1">Delete</div>
+        <h3>My note</h3>
+        <div className="button-1">Submit</div>
+      </div>
+      <div className="note-pad">
+        <span>My note is very interesting</span>
+        <div className="note-footer">
+          {/* <small>04/04/2024</small>
+          <MdDeleteForever className="delete-icon" size="1.3em" /> */}
+        </div>
       </div>
     </div>
   );
